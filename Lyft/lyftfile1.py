261a82ae2495c7d8aa4902885122762e42097c8a
ipsha1545 <ipsha1545@gmail.com>
2016-11-29 22:50:04 -0800

    Adding code to find the most optimized route in Lyft
    Adding the code to find the route with the least estimated costs in lyft among all possible routes

---
+++ Lyft/lyftfile1.py
@@ -0,0 +1,121 @@
+import locationApp
+from locationApp import *
+import requests
+import json
+import itertools
+import a2m.itertools
+import even_more_itertools
+from a2m.itertools import *
+#2control over here
+
+
+
+r = db.engine.execute('select lat from location')
+r2 = db.engine.execute('select longitude from location')
+
+number_of_places = 6
+
+latitudes = []
+
+longitude = []
+
+for i in r:
+    start_lat = str(i['lat'])
+    latitudes.append(start_lat)
+    #print "hi"
+    #print start_lat
+#print latitudes
+
+for i in r2:
+    start_lng = str(i['longitude'])
+    longitude.append(start_lng)
+    #print "hi"
+    #print start_lng
+#print longitude
+
+pl_ind_lat_lng = {
+    "location_no.": 0,
+    "lats": 0,
+    "lngs": 0
+}
+
+for i in range(0,6):
+    pl_ind_lat_lng["location_no."] = i
+    pl_ind_lat_lng["lats"] = latitudes[i]
+    pl_ind_lat_lng["lngs "] = longitude[i]
+    #print pl_ind_lat_lng
+
+
+combo1 = list(itertools.permutations([0,1,3,4,5], 2))
+#print combo1
+#r i in range(0,42):
+   # print combo1[i]
+
+#print "the pairs of addresses are"
+#print combo1[29]
+start_location_lat = latitudes[combo1[19][0]]
+start_location_lng = longitude[combo1[19][0]]
+end_location_lat = latitudes[combo1[19][1]]
+end_location_lng = longitude[combo1[19][1]]
+#print start_location_lat
+#print start_location_lng
+#print end_location_lat
+#print end_location_lng
+
+estimate_cost = []
+
+for i in range(0,20):
+    #print "The pair of addresses are"
+    print combo1[i]
+    print i
+    start_location_lat = latitudes[combo1[i][0]]
+    start_location_lng = longitude[combo1[i][0]]
+    end_location_lat = latitudes[combo1[i][1]]
+    end_location_lng = longitude[combo1[i][1]]
+    #print start_location_lat
+    #print start_location_lng
+    #print end_location_lat
+    #print end_location_lng
+    url = 'https://api.lyft.com/v1/cost'
+    payload = {'start_lat': start_location_lat , 'start_lng': start_location_lng, 'end_lat': end_location_lat,'end_lng':end_location_lng}
+    myheaders = {'Authorization': 'Bearer Auth Token'}
+    r = requests.get(url, headers=myheaders, params=payload)
+    d = json.dumps(r.json())
+    json_acceptable_string = d.replace("'", "\"")
+    data = json.loads(json_acceptable_string)
+    cost = []
+    a1 = (data["cost_estimates"][0]["estimated_cost_cents_max"]/100.0)
+    b1 = (data["cost_estimates"][0]["estimated_cost_cents_min"] / 100.0)
+    c1 = (a1 + b1)/2.0
+    cost.append(c1)
+    p = (data["cost_estimates"][1]["estimated_duration_seconds"]/60.0)
+    minutes = float("{0:.2f}".format(p))
+    distance = (data["cost_estimates"][1]["estimated_distance_miles"])
+    result = {"id":0,
+
+              "start":0,
+
+              "best_route_by_costs":[],
+
+              "providers":[
+                {"name":"Lyft",
+                 "total_costs_by_cheapest_car_type":0,
+                 "total_duration": 0,
+                 "duration_unit": "minute",
+                 "total_distance": 0,
+                 "distance_unit": "mile"
+                 }
+                ],
+              "end":0
+              }
+
+    i = result["providers"][0]["total_costs_by_cheapest_car_type"] = (min(cost))
+    estimate_cost.append(i)
+    result["providers"][0]["total_duration"] = minutes
+    result["providers"][0]["total_distance"] = distance
+    print result
+    print i
+
+for i in range(0,20):
+    print estimate_cost[i]
+
---
+++ Lyft/lyftfile2.py
@@ -0,0 +1,147 @@
+from lyftfile1 import *
+
+combo = list(itertools.permutations([0,1,3,4,5,6], 6))
+#print combo
+
+originallist = [0,1,3,4,5]
+start = [originallist[0]]
+
+end  = originallist[-1]
+
+list1 = []
+
+for i in range (1, len(originallist)-1):
+ list1.append(originallist[i])
+
+
+#print list(itertools.permutations(list1, 3))
+"""for i in range(0,6):
+ routei = list(itertools.permutations(list1, 3))[i]
+ print routei"""
+
+route1 = list(itertools.permutations(list1, 3))[0]
+route2 = list(itertools.permutations(list1, 3))[1]
+route3 = list(itertools.permutations(list1, 3))[2]
+route4 = list(itertools.permutations(list1, 3))[3]
+route5 = list(itertools.permutations(list1, 3))[4]
+route6 = list(itertools.permutations(list1, 3))[5]
+
+print route1
+
+print route2
+
+print route3
+
+print route4
+
+print route5
+
+print route6
+
+
+---------------------------------------------------------------------------1
+routelist1 = []
+
+routelist2 = []
+
+routelist3 = []
+
+routelist4 = []
+
+routelist5 = []
+
+routelist6 = []
+
+print"The possible routes are:-"
+
+for i in range(0,3):
+    routelist1.append(route1[i])
+routelist1.insert(0,originallist[0])
+routelist1.insert(len(routelist1),originallist[len(originallist)-1])
+print routelist1
+
+
+for i in range(0,3):
+    routelist2.append(route2[i])
+routelist2.insert(0,originallist[0])
+routelist2.insert(len(routelist2),originallist[len(originallist)-1])
+print routelist2
+
+for i in range(0,3):
+    routelist3.append(route3[i])
+routelist3.insert(0,originallist[0])
+routelist3.insert(len(routelist3),originallist[len(originallist)-1])
+print routelist3
+
+for i in range(0,3):
+    routelist4.append(route4[i])
+routelist4.insert(0,originallist[0])
+routelist4.insert(len(routelist4),originallist[len(originallist)-1])
+print routelist4
+
+for i in range(0,3):
+    routelist5.append(route5[i])
+routelist5.insert(0,originallist[0])
+routelist5.insert(len(routelist5),originallist[len(originallist)-1])
+print routelist5
+
+for i in range(0,3):
+    routelist6.append(route6[i])
+routelist6.insert(0,originallist[0])
+routelist6.insert(len(routelist6),originallist[len(originallist)-1])
+print routelist6
+
+
+
+print("Considering route1")
+print routelist1
+p1 =  estimate_cost[0]+estimate_cost[5]+estimate_cost[10]+estimate_cost[15]
+print "The total estimated costs for this route is"
+print p1
+
+print("Considering route2")
+print routelist2
+p2 =  estimate_cost[0]+estimate_cost[6]+estimate_cost[14]+estimate_cost[11]
+print "The total estimated costs for this route is"
+print p2
+
+print("Considering route3")
+print routelist3
+p3 =  estimate_cost[1]+estimate_cost[9]+estimate_cost[6]+estimate_cost[15]
+print "The total estimated costs for this route is"
+print p3
+
+print("Considering route4")
+print routelist4
+p4 =  estimate_cost[1]+estimate_cost[10]+estimate_cost[13]+estimate_cost[19]
+print "The total estimated costs for this route is"
+print p4
+
+print("Considering route5")
+print routelist5
+p5 =  estimate_cost[2]+estimate_cost[13]+estimate_cost[5]+estimate_cost[11]
+print "The total estimated costs for this route is"
+print p5
+
+print("Considering route6")
+print routelist6
+p6 =  estimate_cost[2]+estimate_cost[14]+estimate_cost[9]+estimate_cost[7]
+print "The total estimated costs for this route is"
+print p6
+
+mincost = min(p1,p2,p3,p4,p5,p6)
+print ("The minimum cost is  " + repr(mincost))
+
+
+print "As the total estimated costs for route " + repr(routelist3) + " is minimum so route 3 is the most preferred route for the entered list of places"
+
+
+
+
+
+
+
+
+
+
+
