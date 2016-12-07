e84119379f06d03a3e8aa71d42af5a84900514e6
gurnoors <gurnoor@gmail.com>
2016-12-06 21:28:10 -0800

    test response for UI results page
    Signed-off-by: gurnoors <gurnoor@gmail.com>

--- db_and_common/endpoint_for_ui.py
+++ db_and_common/endpoint_for_ui.py
@@ -1,8 +1,7 @@
-from flask import request, json
+from flask import request, json, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 import location_api.location_app
-import trip_planner.planner
 from db_and_common import app
 
 # from db_and_common.__init__ import location
@@ -50,6 +49,129 @@ def parse_and_forward():
 
     # call prabhu's API
     # requests.post(BASE_URI + '/trips', json=trip_json)  # or replace with direct functin call
-    trip_planner.planner.plan(trip_json)
-    return "Hello from locationApp, Gurnoor<br/> Query: " + request.query_string + \
-           "<br/> location 0: " + request.args.get('Location 0')
+
+
+    # resp = trip_planner.planner.plan(trip_json)
+
+    resp = {
+        "best_route_by_costs": [
+            {
+                "latitude": 190401.235,
+                "longitude": 49.44,
+                "address": "/locations/12345 address",
+                "weather": {},
+                "top_crime": [
+                    "street1",
+                    "street2"
+                ],
+                "popular_spots": [
+                    {
+                        "name": "beach",
+                        "link": "http://lonelyplanet.com/beach"
+                    }
+                ]
+            },
+            {
+                "latitude": 190401.235,
+                "longitude": 49.44,
+                "address": "/locations/12345 address"
+            },
+            {
+                "latitude": 190401.235,
+                "longitude": 49.44,
+                "address": "/locations/12345 address"
+            },
+            {
+                "latitude": 190401.235,
+                "longitude": 49.44,
+                "address": "/locations/12345 address"
+            },
+            {
+                "latitude": 190401.235,
+                "longitude": 49.44,
+                "address": "/locations/12345 address"
+            }
+        ],
+        "providers": [
+            {
+                "name": "UberX",
+                "cost": 125,
+                "currency_code": "USD",
+                "total_duration": 640,
+                "duration_unit": "minute",
+                "total_distance": 25.05,
+                "distance_unit": "mile"
+            },
+            {
+                "name": "Lyft",
+                "cost": 110,
+                "currency_code": "USD",
+                "total_duration": 620,
+                "duration_unit": "minute",
+                "total_distance": 25.05,
+                "distance_unit": "mile"
+            },
+            {
+                "name": "UberXL",
+                "cost": 125,
+                "currency_code": "USD",
+                "total_duration": 640,
+                "duration_unit": "minute",
+                "total_distance": 25.05,
+                "distance_unit": "mile"
+            },
+            {
+                "name": "Uber, VTA, tram",
+                "cost": 50,
+                "currency_code": "USD",
+                "total_duration": 800,
+                "duration_unit": "minute",
+                "total_distance": 30.05,
+                "distance_unit": "mile",
+                "legs": [
+                    {
+                        "name": "Uber",
+                        "cost": 15,
+                        "currency_code": "USD",
+                        "total_duration": 20,
+                        "duration_unit": "minute",
+                        "total_distance": 10.05,
+                        "distance_unit": "mile",
+                        "start": {
+                            "latitude": 190401.235,
+                            "longitude": 49.44,
+                            "address": "/locations/12345 address"
+                        },
+                        "end": {
+                            "latitude": 190401.235,
+                            "longitude": 49.44,
+                            "address": "/locations/12345 address"
+                        }
+                    },
+                    {
+                        "name": "VTA",
+                        "cost": 2.5,
+                        "currency_code": "USD",
+                        "total_duration": 80,
+                        "duration_unit": "minute",
+                        "total_distance": 30.05,
+                        "distance_unit": "mile",
+                        "start": {
+                            "latitude": 190401.235,
+                            "longitude": 49.44,
+                            "address": "/locations/12345 address"
+                        },
+                        "end": {
+                            "latitude": 190401.235,
+                            "longitude": 49.44,
+                            "address": "/locations/12345 address"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+    return render_template('result.html', text=resp)
+
+    # return "Hello from locationApp, Gurnoor<br/> Query: " + request.query_string + \
+    #        "<br/> location 0: " + request.args.get('Location 0')
